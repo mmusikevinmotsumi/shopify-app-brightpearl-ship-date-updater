f66b81112b19f1df2a364bc0d5f4eb53b86b16e3
mmusikevinmotsumi <mmusikevinmotsumi@gmail.com>
2024-07-06 04:07:05 -0400

    1) added store details to MySQL, 2) Shopify-BrightPearl-MySQL integration, 3) Shopify app CSS style

--- generate_token.php
+++ generate_token.php
@@ -19,6 +19,17 @@ $params = array_diff_key($params, array('hmac' => '')); // Remove hmac from para
 ksort($params); // Sort params lexicographically
 $computed_hmac = hash_hmac('sha256', http_build_query($params), $shared_secret);
 
+$servername = $_ENV['DBSERVER'];
+$username = $_ENV['DBUSER'];
+$password = $_ENV['DBPW'];
+$dbname = $_ENV['DBNAME'];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 // Use hmac data to check that the response is from Shopify or not
 if (hash_equals($hmac, $computed_hmac)) {
 	// Set variables for our request
@@ -47,6 +58,8 @@ if (hash_equals($hmac, $computed_hmac)) {
 
 	$result = json_decode($result, true);
 	$access_token = $result['access_token'];
+	$shopifyStoreName = $_GET['shop'];
+	$tableName = 'shop_details';
 
 	// Show the access token (don't do this in production!)
     //Here is the place where you can store the access token to the database
@@ -54,10 +67,60 @@ if (hash_equals($hmac, $computed_hmac)) {
 	echo 'App Successfully Installed!<br>';
 	echo 'Access Token:' . $access_token;
 
-	$_SESSION['access_token'] = $access_token;
-	$_SESSION['shop'] = $_GET['shop'];
+	create_token_table($tableName);
+	sync_token_to_database($tableName, $shopifyStoreName, $access_token);
+
 } else {
 	// Someone is trying to be shady!
 	die('This request is NOT from Shopify!');
 }
 
+function create_token_table($tableName)
+{
+    global $conn;
+
+    // Create table
+    $result = $conn->query("SHOW TABLES LIKE '".$tableName."'");
+
+    if ($result->num_rows == 0) {
+        // Create the table if it does not exist
+        $createTableSql = "CREATE TABLE `".$tableName."` (
+            id INT(10) AUTO_INCREMENT PRIMARY KEY,
+            shopify_store_name VARCHAR(255) NOT NULL,
+            access_token VARCHAR(255) NOT NULL,
+            updated_at TIMESTAMP NOT NULL
+        )";
+        
+        // if ($conn->query($createTableSql) === TRUE) {
+        //     echo "Table ".$tableName." created successfully.";
+        // } else {
+        //     echo "Error creating table: " . $mysqli->error;
+        // }
+    } else {
+
+	}
+}
+function sync_token_to_database($tableName, $shopifyStoreName, $access_token)
+{
+    global $conn;
+	$updated_at = date('Y-m-d H:i:s');
+
+	$sql = $conn->query("SELECT * FROM `".$tableName."` WHERE shopify_store_name = `".$shopifyStoreName."`");
+	if ($sql->num_rows > 0) {
+		$update_sql = "UPDATE ".$tableName.
+		"SET 
+		access_token = `".$access_token."`,
+		updated_at = `".$updated_at."`
+		WHERE shopify_store_name = `".$shopifyStoreName."`";
+	}
+	else{
+		$result = $conn->query("INSERT INTO `" . $tableName . "_orders` (" .
+		"shopify_store_name, access_token, updated_at) " .
+		"VALUES (" .
+		"'" . $shopifyStoreName . "', " .
+		"'" . $access_token . "', " .
+		"'" . $updated_at . "')");
+	}
+
+    $conn->close();
+}
--- index.php
+++ index.php
@@ -13,6 +13,7 @@ $shopifyApiKey = $_ENV['SHOPIFY_API_KEY'];
 $shopifyPassword = $_ENV['SHOPIFY_PASSWORD'];
 $shopifyStoreName = $_ENV['SHOPIFY_STORE_NAME'];
 $shopifyToken = $_ENV['SHOPIFY_TOKEN'];
+$tableName = $shopifyStoreName . "_orders";
 
 $brightpearlApiToken = $_ENV['BRIGHTPEARL_API_TOKEN'];
 $brightpearlAccount = $_ENV['BRIGHTPEARL_ACCOUNT'];
@@ -25,18 +26,65 @@ $username = $_ENV['DBUSER'];
 $password = $_ENV['DBPW'];
 $dbname = $_ENV['DBNAME'];
 
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 $shopifyBaseUrl = "https://" . $shopifyStoreName . ".myshopify.com/admin/api/2024-04";
 $brightpearlBaseUrl = "https://use1.brightpearlconnect.com/public-api/".$brightpearlAccount;
 
 $client = new Client();
 
-function getFulfilledOrders($client, $shopifyBaseUrl, $shopifyToken)
+ob_start();
+
+function createOrderTable()
 {
+    global $conn, $tableName;
+
+    // Create table
+    $result = $conn->query("SHOW TABLES LIKE '".$tableName."'");
+
+    if ($result->num_rows == 0) {
+        // Create the table if it does not exist
+        $createTableSql = "CREATE TABLE `".$tableName."` (
+            id INT(10) AUTO_INCREMENT PRIMARY KEY,
+            order_id VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP NOT NULL,
+            shipped_at TIMESTAMP,
+            total_price DECIMAL(10, 2) NOT NULL,
+            financial_status VARCHAR(255) NOT NULL,
+            fulfillment_status VARCHAR(255),
+            brightpearl_ECOMSHIP DATE,
+            brightpearl_GoodsOutNote VARCHAR(10)
+        )";
+        
+        if ($conn->query($createTableSql) === TRUE) {
+            echo "Table '".$tableName."' created successfully.";
+        } else {
+            echo "Error creating table: " . $mysqli->error;
+        }
+    } else {
+        $sql = "TRUNCATE TABLE `".$tableName."`";
+
+        if ($conn->query($sql) === TRUE) {
+            // echo "Table $tableName truncated successfully.";
+        } else {
+            echo "Error truncating table: " . $conn->error;
+        }
+    }
+}
+
+function sync_orders_to_database($client, $shopifyBaseUrl, $shopifyToken)
+{
+    global $conn, $shopifyStoreName;
+
     $orders = [];
     $page = 1;
     $limit = 250;  // You can adjust the limit to the maximum allowed by Shopify (which is 250)
-    $created_at_min = '2023-01-01T00:00:00Z'; // Start of January 2023
-    $created_at_max = '2024-06-24T23:59:59Z'; // End of Jun 24, 2024
+    $created_at_min = '2023-01-01T00:00:00Z'; 
+    $created_at_max = date('Y-m-d H:i:s'); 
 
     $url = $shopifyBaseUrl . "/orders.json?status=any&limit={$limit}&created_at_min={$created_at_min}&created_at_max={$created_at_max}";
 
@@ -84,73 +132,88 @@ function getFulfilledOrders($client, $shopifyBaseUrl, $shopifyToken)
         $page++;
     } while (!empty($data['orders']));
     
-    global $servername;
-    global $username;
-    global $password;
-    global $dbname;
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    
     // Loop through orders and insert into database
     foreach ($orders as $order) {
-        $order_id = $order['id'];
-        $created_at = $order['created_at'];
-        $total_price = $order['total_price'];
+        $order_id = $order['name'];
         $financial_status = $order['financial_status'];
         $fulfillment_status = $order['fulfillment_status'];
-        $sync_to_brightpearl = ''
+        $created_at = $order['created_at'];
+        $shipped_at = isset($order['fulfillments'][0]['created_at']) ? $order['fulfillments'][0]['created_at'] : NULL;
+        $total_price = $order['total_price'];
     
-        $sql = "INSERT INTO {$shopifyStoreName}_orders (order_id, created_at, total_price, financial_status, fulfillment_status, sync_to_brightpearl) VALUES ({$order_id}, {$created_at}, {$total_price}, {$financial_status}, {$fulfillment_status}, {$sync_to_brightpearl})";
-        
-        if ($conn->query($sql) !== TRUE) {
-            echo "Error: " . $sql . "<br>" . $conn->error;
+        if ($fulfillment_status == 'fulfilled') {
+            list($brightpearl_ECOMSHIP, $brightpearl_GoodsOutNote) = update_brightpearl_fields($client, $order_id, $shipped_at);
+        } else {
+            $fulfillment_status = 'unfulfilled';
+            $brightpearl_ECOMSHIP = NULL;
+            $brightpearl_GoodsOutNote = NULL;
         }
+    
+        global $conn;
+    
+        $stmt = $conn->prepare("INSERT INTO `" . $shopifyStoreName . "_orders` (
+            order_id, created_at, shipped_at, total_price, financial_status, fulfillment_status, brightpearl_ECOMSHIP, brightpearl_GoodsOutNote
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+    
+        $stmt->bind_param(
+            "ssssssss",
+            $order_id,
+            $created_at,
+            $shipped_at,
+            $total_price,
+            $financial_status,
+            $fulfillment_status,
+            $brightpearl_ECOMSHIP,
+            $brightpearl_GoodsOutNote
+        );
+    
+        if (!$stmt->execute()) {
+            echo "Error: " . $stmt->error;
+        }
+    
+        $stmt->close();
     }
     
-    $conn->close();
-
-    // Now $orders contains all the orders from January 2023
-    echo 'Total orders fetched: ' . count($orders) . "\n";
-    ?>
-    <table id="leadstbl" style="width:100%;" class="Polaris-IndexTable__Table Polaris-IndexTable__Table--sortable Polaris-IndexTable__Table--sticky"">
-        <thead>
-            <th class="Polaris-IndexTable__TableHeading Polaris-IndexTable__TableHeading--sortable Polaris-IndexTable__TableHeading--first">Order #</th>
-            <th class="Polaris-IndexTable__TableHeading Polaris-IndexTable__TableHeading--second Polaris-IndexTable__TableHeading--sortable">Fulfillment Status</th>
-            <th class="Polaris-IndexTable__TableHeading Polaris-IndexTable__TableHeading--sortable">Ship Date</th>
-            <th class="Polaris-IndexTable__TableHeading Polaris-IndexTable__TableHeading--sortable">BrightPearl Status</th>
-        </thead>
-        <?php
-        foreach ($orders as $order) {
-            if (isset($order['fulfillments'][0])) {
-                $fulfillment = $order['fulfillments'][0];
-                $shipDate = substr($fulfillment['created_at'], 0, 10); // Get only the date part
-            ?>
-                <tr>
-                    <td class="Polaris-IndexTable__TableCell"><?php echo $order['name'] ?></td>
-                    <td class="Polaris-IndexTable__TableCell"><?php echo $fulfillment['status'] ?></td>
-                    <td class="Polaris-IndexTable__TableCell"><?php echo $shipDate ?></td>
-                    <td class="Polaris-IndexTable__TableCell"><?php echo (check_sync($client, $order['name'])) ?></td>
-                </tr>
-            <?php
-                // echo "Order ID: " . $order['id'] . "\n" . "Ship Date: " . $shipDate . "\n";
-                // updateBrightPearlOrder($client, $brightpearlBaseUrl, $brightpearlApiToken, $brightpearlWarehouseId, $order['id'], $shipDate);
-
-            }
-            else{
-            }
-        }
-    ?>
-    </table>
-    <?php
-    // print_r($orders);
+    // $conn->close();
 }
 
-function check_sync($client, $order_id){
+function get_orders_from_database(){
+
+    global $conn, $tableName;
+    $result = $conn->query("SELECT * FROM `" . $tableName . "`");
+    
+    if ($result->num_rows > 0) {
+        // Start the HTML table
+        echo '<div class="table-top-container"><p>Total orders fetched: ' . $result->num_rows . "</p>";
+        echo '<form action="index.php" method="post"><button type="submit" name="resync_orders">Sync All Orders</button></form></div>';
+
+        echo "<table border='1' id='leadstbl'>";
+        echo "<tr><th>ID</th><th>Order ID</th><th>Created At</th><th>Ship Date</th><th>Total Price</th><th>Payment Status</th><th>Fulfillment Status</th><th>Brightpearl PCF_ECOMSHIP</th><th>Brightpearl brightpearl_GoodsOutNote</th></tr>";
+        
+        // Output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row['id'] . "</td>";
+            echo "<td>" . $row['order_id'] . "</td>";
+            echo "<td>" . $row['created_at'] . "</td>";
+            echo "<td>" . $row['shipped_at'] . "</td>";
+            echo "<td>" . $row['total_price'] . "</td>";
+            echo "<td>" . $row['financial_status'] . "</td>";
+            echo "<td>" . $row['fulfillment_status'] . "</td>";
+            echo "<td>" . $row['brightpearl_ECOMSHIP'] . "</td>";
+            echo "<td>" . $row['brightpearl_GoodsOutNote'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    } else {
+        echo '<div class="table-top-container"><p>Total orders fetched: ' . $result->num_rows . "</p>";
+        echo '<form action="index.php" method="post"><button type="submit" name="resync_orders">Sync All Orders</button></form></div>';
+    }
+    
+    
+}
+
+function update_brightpearl_fields($client, $order_id, $shipped_at){
 
     global $brightpearlDevRef;
     global $brightpearlAppRef;
@@ -163,7 +226,7 @@ function check_sync($client, $order_id){
     try {
         $response = $client->request(
             'GET',
-            "{$url}",
+            "{$url}?{$queryParams}",
             [
                 'headers' => [
                     'Authorization' => "Bearer ".$brightpearlApiToken,
@@ -177,43 +240,150 @@ function check_sync($client, $order_id){
         $body = json_decode($response->getBody()->getContents(), true);
 
         // Filter out orders and return the custom field "PCF_ECOMSHIP"
-        return count($body['response']);
-        // $orders = [];
-        // foreach ($body['response'] as $order) {
-        //     if (isset($order['order']['customFields']['PCF_ECOMSHIP'])) {
-        //         $orders[] = [
-        //             'orderId' => $order['order']['id'],
-        //             'PCF_ECOMSHIP' => $order['order']['customFields']['PCF_ECOMSHIP']
-        //         ];
-        //     }
-        // }
-
-        // return count($orders);
-    }
-    catch(Exception $e){
-         echo "Error finding order #:" . $order_id . " in BrightPearl.\n",  $e->getMessage(), "\n";
-    }
-
-    return true;
-}
-
-function updateOrders($client, $shopifyBaseUrl, $brightpearlBaseUrl, $brightpearlApiToken, $brightpearlWarehouseId) {
-
-    $orders = getFulfilledOrders($client, $shopifyBaseUrl);
-
-    foreach ($orders as $order) {
-        if (isset($order['fulfillments'][0])) {
-            $fulfillment = $order['fulfillments'][0];
-            $shipDate = substr($fulfillment['created_at'], 0, 10); // Get only the date part
-            echo "Order ID: " . $order['id'] . "\n" . "Ship Date: " . $shipDate;
-            // updateBrightPearlOrder($client, $brightpearlBaseUrl, $brightpearlApiToken, $brightpearlWarehouseId, $order['id'], $shipDate);
-
+        if(isset($body['response']['results']) && !empty($body['response']['results'])) {
+            foreach($body['response']['results'] as $order) {
+                // The order ID is the first element in the array
+                $orderId = $order[0];
+                return [update_brightpearl_shipdate($client, $orderId, $shipped_at), update_brightpearl_goodsout($client, $orderId, $shipped_at)];
+            }
+        } else {
+            return [NULL, NULL];
         }
     }
+    catch(Exception $e){
+        error_log("Error finding order #:" . $order_id . " in BrightPearl.\n");
+        error_log($e->getMessage(), "\n");
+    }
+
 }
 
-// getFulfilledOrders($client, $shopifyBaseUrl, $shopifyToken);
+function update_brightpearl_shipdate($client, $orderId, $shipped_at) {
+    
+    global $brightpearlDevRef;
+    global $brightpearlAppRef;
+    global $brightpearlBaseUrl;
+    global $brightpearlApiToken;
 
-// getNumberOfOrders($client, $shopifyBaseUrl, $shopifyToken);
+    $url = $brightpearlBaseUrl."/order-service/order/".$orderId."/custom-field";
+    $body = [
+        [
+            "op" => "replace",
+            "path" => "/PCF_ECOMSHIP",
+            "value" => (new DateTime($shipped_at))->format('Y-m-d')
+        ]
+    ];
 
-getFulfilledOrders($client, $shopifyBaseUrl, $shopifyToken);
+    try {
+        $response = $client->request(
+            'PATCH',
+            $url,
+            [
+                'headers' => [
+                    'Authorization' => "Bearer ".$brightpearlApiToken,
+                    'Content-Type' => 'application/json',
+                    'brightpearl-dev-ref' => $brightpearlDevRef,
+                    'brightpearl-app-ref' => $brightpearlAppRef
+                ],
+                'json' => $body
+            ]
+        );
+
+        $result = json_decode($response->getBody()->getContents(), true);
+
+        // Check if PCF_ECOMSHIP was updated successfully
+        if (isset($result['response']['PCF_ECOMSHIP'])) {
+            return $result['response']['PCF_ECOMSHIP'];
+        } else {
+            error_log("Error updating PCF_ECOMSHIP for order #:" . $orderId . " in BrightPearl.\n");
+            error_log($e->getMessage() . "\n");
+            return null; // Handle if PCF_ECOMSHIP wasn't updated
+        }
+
+    } catch (Exception $e) {
+        error_log("Error patching PCF_ECOMSHIP for order #: " . $orderId . " in BrightPearl.\n");
+        error_log($e->getMessage() . "\n");
+        return null; // Handle the error gracefully, possibly log it
+    }
+}
+
+function update_brightpearl_goodsout($client, $orderId, $shipped_at) {
+    
+    global $brightpearlDevRef;
+    global $brightpearlAppRef;
+    global $brightpearlBaseUrl;
+    global $brightpearlApiToken;
+
+    $url = $brightpearlBaseUrl."/warehouse-service/order/" . $orderId . "/goods-note/goods-out";
+
+    try {
+        $response = $client->request(
+            'GET',
+            $url,
+            [
+                'headers' => [
+                    'Authorization' => "Bearer " . $brightpearlApiToken,
+                    'Content-Type' => 'application/json',
+                    'brightpearl-dev-ref' => $brightpearlDevRef,
+                    'brightpearl-app-ref' => $brightpearlAppRef
+                ]
+            ]
+        );
+
+        $result = json_decode($response->getBody()->getContents(), true);
+
+        // Check if PCF_ECOMSHIP was updated successfully
+        if (isset($result['response'][0]['status']['shipped']) && $result['response'][0]['status']['shipped'] === true) {
+            return 'shipped';
+        } else {
+            $url = $brightpearlBaseUrl."/warehouse-service/goods-note/goods-out/" . $orderId . "/event";
+            $body = [
+                "events" => [
+                    [
+                        "eventCode" => "SHW",
+                        "occured" => $shipped_at,
+                        "eventOwnerId" => 74672
+                    ]
+                ]
+            ];
+            try {
+                $response = $client->request(
+                    'POST',
+                    $url,
+                    [
+                        'headers' => [
+                            'Authorization' => "Bearer ".$brightpearlApiToken,
+                            'Content-Type' => 'application/json',
+                            'brightpearl-dev-ref' => $brightpearlDevRef,
+                            'brightpearl-app-ref' => $brightpearlAppRef
+                        ],
+                        'json' => $body
+                    ]
+                );
+
+                return 'shipped';
+            } catch (Exception $e) {
+                error_log("Error updating Goods-Out Note for order #:" . $orderId . " in BrightPearl.\n");
+                error_log($e->getMessage() . "\n");
+                return NULL; 
+            }
+        }
+
+    } catch (Exception $e) {
+        error_log("Error fetching Goods-Out Note for order #: " . $orderId . " in BrightPearl.\n");
+        error_log($e->getMessage() . "\n");
+        return NULL; 
+    }
+}
+
+createOrderTable();
+get_orders_from_database();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['resync_orders'])) {
+    // Call the PHP function
+    ob_clean();
+    createOrderTable();
+    sync_orders_to_database($client, $shopifyBaseUrl, $shopifyToken);
+    get_orders_from_database();
+}
+
+$conn->close();
